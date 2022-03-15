daf91b85e8066cacbf124a1989538661772cdc74
rar4 <66429342+rar4@users.noreply.github.com>
2022-03-15 16:04:53 +0200

    Update c.py

--- c.py
+++ c.py
@@ -1,3 +1,4 @@
+import telegram.ext.filters
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from pprint import pprint
 
@@ -7,59 +8,24 @@ dispatcher = updater.dispatcher
 print('Bot started.')
 def start(update, context):
     chat = update.effective_chat
-    a = 'hello, welcome to calculator bot input /c and write an equation with spaces (like /c 1 + 1)' \
-        'operators are: +  -  *  /  //(integer division) %(remainder) **(degree)'
+    a = 'Hello, welcome to calculator bot, input a quotation please'
     context.bot.send_message(chat_id=chat.id, text=a)
 
 
 def ca(s, b, action):
     if action == "+":
-        print(s + b)
         f = s + b
+        if f % 1 == 0:
+            f = int(f)
     elif action == "-":
-        print(s - b)
         f = s - b
+        if f % 1 == 0:
+            f = int(f)
     elif action == "*":
-        print(s * b)
         f = s * b
+        if f % 1 == 0:
+            f = int(f)
     elif action == "/":
-        print(s / b)
-        f = s / b
-    elif action == "//":
-        f = s // b
-        print(s // b)
-    elif action == "%":
-        print(s % b)
-        f = s % b
-    elif action == "**":
-        print(s ** b)
-        f = s ** b
-
-    else:
-        return "error"
-    return f
-
-def ma(update, context):
-    chat = update.effective_chat
-    l = update.message.text
-    try:
-        a, b, c, d = l.split()
-    except ValueError:
-        context.bot.send_message(chat_id=chat.id, text='invalid equation')
-        return
-    if b.isdigit():
-        v = float(b)
-    else:
-        context.bot.send_message(chat_id=chat.id, text='invalid equation')
-    if d.isdigit():
-        n = float(d)
-    else:
-        context.bot.send_message(chat_id=chat.id, text='invalid equation')
-    g = ca(v, n, c)
-    context.bot.send_message(chat_id=chat.id, text=g)
-
-
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('c', ma))
-updater.start_polling()
-updater.idle()
+        if b!= 0:
+            f = s / b
+        else:
