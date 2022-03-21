70cb71825a4bb1b44c8eb8280926bb058e130705
rar4 <66429342+rar4@users.noreply.github.com>
2022-03-21 21:53:48 +0100

    Update іві.py

--- іві.py
+++ іві.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import *
 import json as d
 import datetime
 TOKEN = '5269119122:AAHUTCgZkvZ6Nwu01D6K4AI800HZ85Bz8CI'
@@ -6,43 +6,41 @@ print("Bot is bot")
 updater = Updater(TOKEN)
 
 
-def ah(update, context):
+def wel(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Hello! I am your echo bot')
+
+
+def echo(update, context):
     chat = update.effective_chat
     mas = update.message.text
     context.bot.send_message(chat_id=chat.id, text=mas)
-    lol(update)
+    create_json(update)
 
 
+def js_write(sus):
+    with open('sus.json', 'a') as f:
+        d.dump(sus, f)
 
-def lol(update):
-    a = datetime.datetime.now()
-    sus =  {
-        """inf""" : {
-        "user" : {"user id" : update.message.chat.id, "user name" : update.message.chat.first_name},
-        "message": {"text": update.message.text, "id": update.message.message_id},
-        "date": a
-    }}
-    try:
-        with open('sus.json', 'r') as g:
-            ss = str(f""", {sus}]""")
-            jj = 9
-    except:
-        ss = str(f"""[{sus}]""")
-        jj = 0
-    ss = ss.replace("'", '"')
-    ss = ss.replace('datetime.datetime', '')
-    ss = ss.replace('(', '[')
+
+def dat():
+    a = str(datetime.datetime.now())
+    a = a.replace('datetime.datetime', '')
+    ss = a.replace('(', '[')
     ss = ss.replace(')', ']')
-    with open('sus.json', 'a+') as g:
-        g.close()
-    with open('sus.json', 'r+') as g:
-        o = g.readline()
-        if jj != 0:
-            g.seek(len(o) - 1)
-        g.write(ss)
+    return ss
+
+def create_json(update):
+    js_obj = {
+        "user": {"chat id": update.message.chat.id, "user name" : update.message.chat.first_name},
+        "message": {"text": update.message.text, "id": update.message.message_id},
+        "date": dat()
+    }
+    js_write(js_obj)
 
 
 disp = updater.dispatcher
-disp.add_handler(MessageHandler(Filters.all, ah))
+disp.add_handler(CommandHandler('start', wel))
+disp.add_handler(MessageHandler(Filters.text, echo))
 updater.start_polling()
 updater.idle()
